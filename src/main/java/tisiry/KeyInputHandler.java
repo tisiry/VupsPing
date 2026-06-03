b26dac7044882811db7f3fa1aba7890864df208a
tisiry <nik677050@gmail.com>
2026-06-03 13:50:58 +0300

    save

---
+++ src/main/java/tisiry/KeyInputHandler.java
@@ -0,0 +1,26 @@
+package tisiry;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyInputHandler {
+    public static KeyBinding moveHudKey;
+
+    public static void register() {
+        moveHudKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Настройка HUD",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_K,
+                "VupsPing HUD"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player != null && moveHudKey.wasPressed()) {
+                client.setScreen(new MoveHudScreen(null));
+            }
+        });
+    }
+}
